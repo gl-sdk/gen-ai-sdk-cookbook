809a7591ff5e27620ccecf842f198231b0cd6299
felixkurniawan <felix.k.tirto@gdplabs.id>
2026-03-04 16:16:59 +0700

    Separate AIP Cookbook

--- gl-aip/examples/hello-world/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Hello World Single Agent - Tools module."""
-
-from tools.greeting import GreetingTool
-from glaip_sdk.tools import Tool
-
-time_tool = Tool.from_native("time_tool")
-
-__all__ = ["GreetingTool", time_tool]
--- gl-aip/examples/runtime-config/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Runtime Config Demo - Tools module."""
-
-from tools.research_formatter import ResearchFormatterTool
-
-__all__ = ["ResearchFormatterTool"]
